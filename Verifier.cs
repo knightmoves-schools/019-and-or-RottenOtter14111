ec9428be312d740f5d5f5902055177780a332788
RottenOtter14111 <arogers@anamosa.k12.ia.us>
2026-02-12 21:24:11 -0600

    done??
    Added properties and methods to determine tax status and vehicle type.

--- Verifier.cs
+++ Verifier.cs
@@ -2,4 +2,42 @@
 public class Verifier
 {
     // Add your code here
+    public bool taxExempt = false;
+    public string state = "IA";
+    public bool hasWheels = false;
+    public bool canFly = false;
+    public bool canSwim = false;
+    public string Taxable(state taxExempt){
+        if(state == "IA" && taxExempt == false){
+            return "is taxable";
+        }
+        if(state == "IA" && taxExempt == true){
+            return "";
+        }
+        if(state == "FL" && taxExempt == false){
+            return "";
+        }
+    }
 }
+public string isVehicle(hasWheels canFly canSwim){
+    if(hasWheels == true || canFly == true || canSwim == true){
+        return "is vehicle";
+    }
+    if(hasWheels == true && canFly == false && canSwim == false){
+        return "is vehicle";
+    }
+    if(hasWheels == false && canFly == true && canSwim == false){
+        return "is vehicle";
+    }
+    if(hasWheels == false && canFly == false && canSwim == true){
+        return "is vehicle";
+    }
+    if(hasWheels == false && canFly == true && canSwim == true){
+        return "is vehicle";
+    }
+    if(hasWheels == false && canFly == false && canSwim == true){
+        return "";
+    }
+    
+}
+
